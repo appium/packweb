31f7736bc1adf3f7df8ecab27e3c63d3df7b1157
Jonathan Lipps <jlipps@gmail.com>
2015-05-14 17:32:31 -0700

    show ownership data in a nice cli with --show

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // transpile:main
 
+import { asyncify } from 'asyncbox';
 import { default as PackWeb } from './lib/packweb';
+import { default as cli } from './lib/cli';
+
+if (module === require.main) {
+  asyncify(cli);
+}
 
 export { PackWeb };
-
